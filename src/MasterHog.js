1cd1c811bdf8edf9632cef9e510604c125edd104
michermd <michermd@gmail.com>
2018-08-08 10:35:47 -0400

    Done.

--- src/MasterHog.js
+++ src/MasterHog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Master from './assets/master-hog.png'
 import BabyHog from './BabyHog'
-// import offspring from './db.js'
+import offspring from './db.js'
 
 export default class MasterHog extends Component {
 
@@ -21,6 +21,11 @@ export default class MasterHog extends Component {
 
 
   render() {
+    offspring.forEach((pig) => {
+      console.log(pig.name);
+    })
+
+
     return (
       <div>
 
@@ -30,19 +35,19 @@ export default class MasterHog extends Component {
         Sun<br></br>
         <input type="radio" name="eyeColor" value="glowing" onChange={this.changeEyeColor} />
         Glowing<br></br>
-      
+
         <h2>Name: Master Blaster</h2>
         <h3>Weight: 2.54 Tons</h3>
         <h3>Eye Color: {this.state.eyeColor}</h3>
-        
+
         <div id="masters-domicile">
           <img id="master-blaster" src={Master} alt="" />
         </div>
-        
+
         <ul className="hoglist">
-          <BabyHog />
-          <BabyHog />
-          <BabyHog />
+          <BabyHog name={offspring[0].name} hobby={offspring[0].hobby} eyeColor={this.state.eyeColor}/>
+          <BabyHog name={offspring[1].name} hobby={offspring[1].hobby} eyeColor={this.state.eyeColor}/>
+          <BabyHog name={offspring[2].name} hobby={offspring[2].hobby} eyeColor={this.state.eyeColor}/>
         </ul>
 
       </div>
