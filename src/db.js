919c00bcaa8c395923b5a5f98a5da5daf11298d1
QMaximillian <quinnlashinsky@gmail.com>
2018-08-08 09:54:32 -0400

    Done.

--- src/db.js
+++ src/db.js
@@ -1,15 +1,16 @@
 var offspring = [
-  { 
+  {
     "id": 1,
     "name": "JeffKatzy Katz",
-    "weight": "Never gonna give"
+    "hobby": "Never gonna give"
+
   },
-  { 
+  {
     "id": 2,
     "name": "Rachel",
-    "weight": "You up, never gonna"
+    "hobby": "You up, never gonna"
   },
-  { 
+  {
     "id": 3,
     "name": "Maxwell",
     "hobby": "let you down"
